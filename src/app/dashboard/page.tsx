e9570542b589fcf06de9f2ad08379b054c6ea1b0
ShahAlom47 <sahalom4729@gmail.com>
2026-02-26 19:51:21 +0600

    added reuseble v=category select componet

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+"use client"
+import { useCategories } from '@/hook/useCategory';
+
 
 const DashHome = () => {
+  const { categories, isLoading, isError } = useCategories();
+
+  console.log(categories, isLoading, isError);
   return (
     <div>
 
