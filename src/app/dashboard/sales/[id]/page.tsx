0fb1d458ea5f65ae7878965db0f1cfbffa94b9d2
ShahAlom47 <sahalom4729@gmail.com>
2026-03-13 22:38:25 +0600

    update

--- src/app/dashboard/sales/[id]/page.tsx
+++ src/app/dashboard/sales/[id]/page.tsx
@@ -1,9 +1,16 @@
 'use client'
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import React from 'react';
 
 const SaleDetails = () => {
 const {id}= useParams()
+const {}=useQuery({
+   queryKey:[] ,
+   queryFn:()=>{
+    
+   }
+})
 console.log(id)
     return (
         <div>
