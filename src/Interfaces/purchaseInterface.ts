55b74eda2b62fa5884bbfdbd060ccbfa807b3eea
ShahAlom47 <sahalom4729@gmail.com>
2026-04-01 17:12:37 +0600

    added  purces interface

---
+++ src/Interfaces/purchaseInterface.ts
@@ -0,0 +1,25 @@
+export interface PurchaseMemo {
+  shopName?: string;      // shop name (optional)
+  memoNumber: string;     // memo no
+  amount: number;         // ওই দোকানের total
+}
+
+export interface Purchase {
+  _id?: string;
+
+  date: Date;
+
+  memos: PurchaseMemo[];  // multiple shop memo
+
+  productTotal: number;   // সব memo এর total
+
+  transportCost?: number; // transport / extra cost
+
+  otherCost?: number;     // extra expense (optional)
+
+  grandTotal: number;     // final total (product + cost)
+
+  note?: string;
+
+  createdAt?: Date;
+}
